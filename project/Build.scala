7d279ff39d0f44258d8a8b51b8f06baa3424d09b
Jingwen Owen Ou <jingweno@gmail.com>
2012-08-26 10:27:39 -0700

    Fix name

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-  val appName = "todolist"
+  val appName = "ToDo"
   val appVersion = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
