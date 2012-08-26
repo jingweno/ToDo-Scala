ea2a8ed5ccbb6f8d5d9acc5387a95e329e95ad54
Jingwen Owen Ou <jingweno@gmail.com>
2012-08-26 10:25:02 -0700

    Add apiTasks spec.

--- test/controllers/ApplicationSpec.scala
+++ test/controllers/ApplicationSpec.scala
@@ -1,10 +1,15 @@
 import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
+import org.squeryl.PrimitiveTypeMode.inTransaction
 
 import controllers.routes
+import models.Database
+import models.Task
 import play.api.test.FakeApplication
 import play.api.test.FakeRequest
+import play.api.test.Helpers.OK
 import play.api.test.Helpers.SEE_OTHER
+import play.api.test.Helpers.contentAsString
 import play.api.test.Helpers.inMemoryDatabase
 import play.api.test.Helpers.redirectLocation
 import play.api.test.Helpers.running
@@ -17,6 +22,16 @@ class ApplicationSpec extends FlatSpec with ShouldMatchers {
       val result = controllers.Application.newTask(FakeRequest().withFormUrlEncodedBody("label" -> "FooBar"))
       status(result) should equal(SEE_OTHER)
       redirectLocation(result) should equal(Some(routes.Application.tasks.url))
+    }
+  }
+
+  "A request to the apiTasks Action" should "respond with data" in {
+    running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+      inTransaction(Database.taskTable insert Task("foo"))
+
+      val result = controllers.Application.apiTasks(FakeRequest())
+      status(result) should equal (OK)
+      contentAsString(result) should include ("foo")
     }
   }
 }
